5858ed5db382dc901dbf36eac97751d20605dca4
Alexander Kacharava <alexander@cube.nl>
2024-09-02 14:17:42 +0200

    Remove button type

--- src/components/NoProjectSelected/NoProjectSelected.tsx
+++ src/components/NoProjectSelected/NoProjectSelected.tsx
@@ -9,7 +9,7 @@ export default function NoProjectSelected() {
             <h2 className="text-xl font-bold text-stone-500 my-4">No Project Selected</h2>
             <p className="text-stone-400 mb-4">Select a project OR start a new one</p>
             <p className="mt-8">
-                <Button label="Create new project" buttonType="primary" />
+                <Button label="Create new project" />
             </p>
         </div>
     )
